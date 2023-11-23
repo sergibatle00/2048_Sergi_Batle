fa78a8d5dae0c858ce7fa0b311433503ba9302dd
Sergi <sergibatle00@gmail.com>
2023-11-23 21:08:25 +0100

    new version

--- app/src/main/java/com/example/a2048_sergi_batle/Game1Activity.java
+++ app/src/main/java/com/example/a2048_sergi_batle/Game1Activity.java
@@ -1,5 +1,9 @@
 package com.example.a2048_sergi_batle;
+
+import android.content.Intent;
 import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
@@ -7,132 +11,355 @@ import android.graphics.drawable.ShapeDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-public class Game1Activity extends AppCompatActivity {
-    private ArrayList<TextView> cells;
-    private TextView cell_rectangle_0;
-    private TextView cell_rectangle_1;
-    private TextView cell_rectangle_2;
-    private TextView cell_rectangle_3;
-    private TextView cell_rectangle_4;
-    private TextView cell_rectangle_5;
-    private TextView cell_rectangle_6;
-    private TextView cell_rectangle_7;
-    private TextView cell_rectangle_8;
-    private TextView cell_rectangle_9;
-    private TextView cell_rectangle_10;
-    private TextView cell_rectangle_11;
-    private TextView cell_rectangle_12;
-    private TextView cell_rectangle_13;
-    private TextView cell_rectangle_14;
-    private TextView cell_rectangle_15;
+public class Game1Activity extends AppCompatActivity implements View.OnTouchListener {
+    Button goBack;
+    private int[][] game = new int[4][4];
+    LinearLayout mainLayout;
+    GridLayout gridLayout;
+    TextView gameTitle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game1);
-        cells = new ArrayList<>();
-        cell_rectangle_0 = findViewById(R.id.tv_00);
-        cell_rectangle_1 = findViewById(R.id.tv_01);
-        cell_rectangle_2 = findViewById(R.id.tv_02);
-        cell_rectangle_3 = findViewById(R.id.tv_03);
-        cell_rectangle_4 = findViewById(R.id.tv_04);
-        cell_rectangle_5 = findViewById(R.id.tv_05);
-        cell_rectangle_6 = findViewById(R.id.tv_06);
-        cell_rectangle_7 = findViewById(R.id.tv_07);
-        cell_rectangle_8 = findViewById(R.id.tv_08);
-        cell_rectangle_9 = findViewById(R.id.tv_09);
-        cell_rectangle_10 = findViewById(R.id.tv_10);
-        cell_rectangle_11 = findViewById(R.id.tv_11);
-        cell_rectangle_12 = findViewById(R.id.tv_12);
-        cell_rectangle_13 = findViewById(R.id.tv_13);
-        cell_rectangle_14 = findViewById(R.id.tv_14);
-        cell_rectangle_15 = findViewById(R.id.tv_15);
+        generateLayout();
 
-        cells.add(cell_rectangle_0);
-        cells.add(cell_rectangle_1);
-        cells.add(cell_rectangle_2);
-        cells.add(cell_rectangle_3);
-        cells.add(cell_rectangle_4);
-        cells.add(cell_rectangle_5);
-        cells.add(cell_rectangle_6);
-        cells.add(cell_rectangle_7);
-        cells.add(cell_rectangle_8);
-        cells.add(cell_rectangle_9);
-        cells.add(cell_rectangle_10);
-        cells.add(cell_rectangle_11);
-        cells.add(cell_rectangle_12);
-        cells.add(cell_rectangle_13);
-        cells.add(cell_rectangle_14);
-        cells.add(cell_rectangle_15);
-        asignarValoresATextViews();
-    }
+        gridLayout.setOnTouchListener(new View.OnTouchListener() {
+            private float xStart, yStart, xEnd, yEnd;
+            private static final int MIN_DISTANCE = 150;
 
-    private void asignarValoresATextViews() {
-        for (TextView cell : cells) {
-            asignarValorATextView(cell);
-        }
-    }
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        xStart = event.getX();
+                        yStart = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        xEnd = event.getX();
+                        yEnd = event.getY();
 
-    private void asignarValorATextView(TextView cell) {
-        ColorStateList color;
+                        float deltaX = xEnd - xStart;
+                        float deltaY = yEnd - yStart;
 
-        Drawable background = cell.getBackground();
-        int colorValue = 0;
-
-        if (background instanceof ShapeDrawable) {
-            // Haz algo si el background es ShapeDrawable
-        } else if (background instanceof GradientDrawable) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                color = ((GradientDrawable) background).getColor();
-                int[][] states = new int[][] {
-                        new int[] { android.R.attr.state_pressed},
-                        new int[] { -android.R.attr.state_pressed}
-                };
-                colorValue = color.getColorForState(states[0], color.getDefaultColor());
+                        // Detectar deslizamiento horizontal
+                        if (Math.abs(deltaX) > MIN_DISTANCE) {
+                            if (xEnd > xStart) {
+                                // Deslizamiento hacia la derecha
+                                Log.d("Swipe", "Right");
+                                updateArray("right");
+                            } else {
+                                // Deslizamiento hacia la izquierda
+                                Log.d("Swipe", "Left");
+                                updateArray("left");
+                            }
+                        }
+                        // Detectar deslizamiento vertical
+                        else if (Math.abs(deltaY) > MIN_DISTANCE) {
+                            if (yEnd > yStart) {
+                                // Deslizamiento hacia abajo
+                                Log.d("Swipe", "Down");
+                                updateArray("down");
+                            } else {
+                                // Deslizamiento hacia arriba
+                                Log.d("Swipe", "Up");
+                                updateArray("up");
+                            }
+                        }
+                        break;
+                }
+                updateLayout();
+                return true;
             }
-        } else if (background instanceof ColorDrawable) {
-            colorValue = ((ColorDrawable) background).getColor();
-        }
+        });
+    }
 
-        String colorHex = String.format("#%06X", (0xFFFFFF & colorValue));
-        Log.i("Color hexadecimal: ", colorHex);
 
-        switch (colorHex) {
-            case "#EEE4DA":
-                cell.setText("2");
+    private void startGame() {
+        int two = (int) (int) (Math.random() * 2) + 1;;
+        int four = (int) (Math.random() * 1) + 1;
+        boolean exit = false;
+        while (two>=0) {
+            int randomRow = (int) (Math.random() * 4);
+            int randomCol = (int) (Math.random() * 4);
+            if (game[randomRow][randomCol] == 0) {
+                game[randomRow][randomCol] = 2;
+                exit = true;
+            }
+            two--;
+        }
+        while (four>=0) {
+            int randomRow = (int) (Math.random() * 4);
+            int randomCol = (int) (Math.random() * 4);
+            if (game[randomRow][randomCol] == 0) {
+                game[randomRow][randomCol] = 4;
+                exit = false;
+            }
+            four--;
+        }
+    }
+
+
+    private void updateArray(String action) {
+        switch (action) {
+            case "up":
+                updateArrayUp();
                 break;
-            case "#EDE0C8":
-                cell.setText("4");
+            case "down":
+                updateArrayDown();
                 break;
-            case "#F2B179":
-                cell.setText("8");
+            case "right":
+                updateArrayRight();
                 break;
-            case "#F59563":
-                cell.setText("16");
-                break;
-            case "#F67C5F":
-                cell.setText("32");
-                break;
-            case "#F65E3B":
-                cell.setText("64");
-                break;
-            case "#EDCF72":
-                cell.setText("128");
-                break;
-            case "#EDCC61":
-                cell.setText("256");
-                break;
-            case "#EDC850":
-                cell.setText("512");
-                break;
-            case "#EDC53F":
-                cell.setText("1024");
+            case "left":
+                updateArrayLeft();
                 break;
         }
+        visualizarArrayEnLogcat();
+    }
+
+
+    private void updateLayout() {
+        gridLayout.removeAllViews(); // Elimina todas las vistas actuales del GridLayout
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                final int row = i;
+                final int col = j;
+
+                TextView textView = new TextView(this);
+                textView.setLayoutParams(new GridLayout.LayoutParams(
+                        GridLayout.spec(GridLayout.UNDEFINED, 1f),
+                        GridLayout.spec(GridLayout.UNDEFINED, 1f)));
+                textView.setHeight(250);
+                textView.setWidth(100);
+                textView.setTypeface(null, Typeface.BOLD);
+
+                GridLayout.LayoutParams params = (GridLayout.LayoutParams) textView.getLayoutParams();
+                params.setMargins(20, 15, 20, 15);
+                textView.setLayoutParams(params);
+
+                textView.setBackground(getResources().getDrawable(R.drawable.cell_rectangle));
+                textView.setTextSize(20);
+                textView.setTextColor(Color.BLACK);
+                textView.setGravity(android.view.Gravity.CENTER);
+                textView.setId(View.generateViewId());
+
+                // Verifica si el elemento del array game no es cero y establece el texto
+                if (game[i][j] != 0) {
+                    textView.setText(String.valueOf(game[i][j]));
+
+                    switch (game[i][j]) {
+                        case 2:
+                            textView.setBackground(getResources().getDrawable(R.drawable.cell_rectangle_2));
+                            break;
+                        case 4:
+                            textView.setBackground(getResources().getDrawable(R.drawable.cell_rectangle_4));
+                            break;
+                        // Agrega más casos según los valores de tu juego
+                    }
+                }
+                gridLayout.addView(textView); // Agrega el TextView al GridLayout
+            }
+        }
+    }
+
+
+
+    private void updateArrayUp() {
+        boolean repeat = true;
+
+        while (repeat) {
+            repeat = false;
+
+            for (int j = 0; j < 4; j++) { // Recorre todas las columnas
+                for (int i = 1; i < 4; i++) { // Comienza desde el segundo índice y recorre hacia abajo
+                    if (game[i][j] != 0 && game[i - 1][j] == 0) { // Si el elemento actual no es cero y el de arriba es cero
+                        int aux = game[i - 1][j]; // Intercambia los valores para mover hacia arriba
+                        game[i - 1][j] = game[i][j];
+                        game[i][j] = aux;
+                        repeat = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    private void updateArrayDown() {
+        boolean repeat = true;
+
+        while (repeat) {
+            repeat = false;
+
+            for (int j = 0; j < 4; j++) { // Recorre todas las columnas
+                for (int i = 2; i >= 0; i--) { // Comienza desde el penúltimo índice y recorre hacia arriba
+                    if (game[i][j] != 0 && game[i + 1][j] == 0) { // Si el elemento actual no es cero y el de abajo es cero
+                        int aux = game[i + 1][j]; // Intercambia los valores para mover hacia abajo
+                        game[i + 1][j] = game[i][j];
+                        game[i][j] = aux;
+                        repeat = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void updateArrayRight() {
+        boolean repeat = true;
+
+        while (repeat) {
+            repeat = false;
+
+            for (int i = 0; i < 4; i++) { // Recorre todas las filas
+                for (int j = 2; j >= 0; j--) { // Comienza desde la penúltima columna y se mueve hacia la izquierda
+                    if (game[i][j] != 0 && game[i][j + 1] == 0) { // Si el elemento actual no es cero y el de la derecha es cero
+                        int aux = game[i][j + 1]; // Intercambia los valores para mover hacia la derecha
+                        game[i][j + 1] = game[i][j];
+                        game[i][j] = aux;
+                        repeat = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void updateArrayLeft() {
+        boolean repeat = true;
+
+        while (repeat) {
+            repeat = false;
+
+            for (int i = 0; i < 4; i++) { // Recorre todas las filas
+                for (int j = 1; j < 4; j++) { // Comienza desde la segunda columna y se mueve hacia la derecha
+                    if (game[i][j] != 0 && game[i][j - 1] == 0) { // Si el elemento actual no es cero y el de la izquierda es cero
+                        int aux = game[i][j - 1]; // Intercambia los valores para mover hacia la izquierda
+                        game[i][j - 1] = game[i][j];
+                        game[i][j] = aux;
+                        repeat = true;
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void generateLayout() {
+        startGame();
+
+        mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        mainLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        mainLayout.setGravity(Gravity.CENTER_VERTICAL);
+
+        gameTitle = new TextView(this);
+        gameTitle.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        gameTitle.setText("2048");
+        gameTitle.setTextSize(50);
+        gameTitle.setTextColor(Color.BLACK);
+        gameTitle.setPadding(100, 10, 10, 50);
+        mainLayout.addView(gameTitle);
+
+        gridLayout = new GridLayout(this);
+        gridLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        gridLayout.setRowCount(4);
+        gridLayout.setColumnCount(4);
+        gridLayout.setPadding(10, 10, 10, 10);
+        gridLayout.setBackground(getResources().getDrawable(R.drawable.game1background_rectangle));
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+
+                TextView textView = new TextView(this);
+                textView.setLayoutParams(new GridLayout.LayoutParams(
+                        GridLayout.spec(GridLayout.UNDEFINED, 1f),
+                        GridLayout.spec(GridLayout.UNDEFINED, 1f)));
+                textView.setHeight(250);
+                textView.setWidth(100);
+                textView.setTypeface(null, Typeface.BOLD);
+
+                GridLayout.LayoutParams params = (GridLayout.LayoutParams) textView.getLayoutParams();
+                params.setMargins(20, 15, 20, 15);
+                textView.setLayoutParams(params);
+
+                textView.setBackground(getResources().getDrawable(R.drawable.cell_rectangle));
+                textView.setTextSize(20);
+                textView.setTextColor(Color.BLACK);
+                textView.setGravity(android.view.Gravity.CENTER);
+                textView.setId(View.generateViewId());
+
+                // Verifica si el elemento del array game no es cero y establece el texto
+                if (game[i][j] != 0) {
+                    textView.setText(String.valueOf(game[i][j]));
+
+                    switch (game[i][j]) {
+                        case 2:
+                            textView.setBackground(getResources().getDrawable(R.drawable.cell_rectangle_2));
+                            break;
+                        case 4:
+                            textView.setBackground(getResources().getDrawable(R.drawable.cell_rectangle_4));
+                            break;
+                    }
+                }
+
+                gridLayout.addView(textView);
+            }
+        }
+        mainLayout.addView(gridLayout);
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        buttonParams.setMargins(100, 0, 0, 0);
+        goBack = new Button(this);
+        goBack.setLayoutParams(buttonParams);
+        goBack.setText("GO BACK");
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Game1Activity.this, MainActivity.class));
+            }
+        });
+
+        mainLayout.addView(goBack);
+        setContentView(mainLayout);
+    }
+
+
+    private void visualizarArrayEnLogcat() {
+        StringBuilder arrayStr = new StringBuilder();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arrayStr.append(game[i][j]).append(" ");
+            }
+            arrayStr.append("\n");
+        }
+        Log.d("Array 2048", arrayStr.toString());
+    }
+
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
     }
 }
--- app/src/main/java/com/example/a2048_sergi_batle/Game2Activity.java
+++ app/src/main/java/com/example/a2048_sergi_batle/Game2Activity.java
@@ -1,15 +1,160 @@
 package com.example.a2048_sergi_batle;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class Game2Activity extends AppCompatActivity {
+    private int[][] game = new int[7][7];
+    Button goBack;
+    GridLayout gridLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game2);
+        createLayout();
+
+        visualizarArrayEnLogcat();
     }
 
+
+    private void startGame() {
+        int[][] posiciones = {
+                {0, 0}, {0, 1}, {0, 5}, {0, 6},
+                {1, 0}, {1, 1}, {1, 5}, {1, 6},
+                {5, 0}, {5, 1}, {5, 5}, {5, 6},
+                {6, 0}, {6, 1}, {6, 5}, {6, 6}
+        };
+        for (int[] position : posiciones) {
+            int fila = position[0];
+            int columna = position[1];
+
+            int index = fila * gridLayout.getColumnCount() + columna;
+
+            if (index >= 0 && index < gridLayout.getChildCount()) {
+                View view = gridLayout.getChildAt(index);
+                TextView textView = (TextView) view;
+                textView.setLayoutParams(new GridLayout.LayoutParams(
+                        GridLayout.spec(GridLayout.UNDEFINED, 1f),
+                        GridLayout.spec(GridLayout.UNDEFINED, 1f)));
+                textView.setHeight(150);
+
+                GridLayout.LayoutParams params = (GridLayout.LayoutParams) textView.getLayoutParams();
+                params.setMargins(3, 3, 3, 3);
+                textView.setLayoutParams(params);
+
+                textView.setBackground(getResources().getDrawable(R.drawable.game2background_rectangle));
+                textView.setText("");
+                textView.setGravity(android.view.Gravity.CENTER);
+                textView.setOnClickListener(null);
+            }
+        }
+    }
+
+
+    private void createLayout() {
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.setOrientation(LinearLayout.VERTICAL);
+        mainLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT));
+        mainLayout.setGravity(Gravity.CENTER_VERTICAL);
+
+        TextView gameTitle = new TextView(this);
+        gameTitle.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        gameTitle.setText("Senku");
+        gameTitle.setTextSize(50);
+        gameTitle.setTextColor(Color.BLACK);
+        gameTitle.setPadding(100, 10, 10, 50);
+        mainLayout.addView(gameTitle);
+
+        gridLayout = new GridLayout(this);
+        gridLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        gridLayout.setRowCount(7);
+        gridLayout.setColumnCount(7);
+        gridLayout.setPadding(10, 10, 10, 10);
+        gridLayout.setBackground(getResources().getDrawable(R.drawable.game2background_rectangle));
+
+        for (int i = 0; i < 49; i++) {
+            final int row = i / 7;
+            final int col = i % 7;
+
+            TextView textView = new TextView(this);
+            textView.setLayoutParams(new GridLayout.LayoutParams(
+                    GridLayout.spec(GridLayout.UNDEFINED, 1f),
+                    GridLayout.spec(GridLayout.UNDEFINED, 1f)));
+            textView.setHeight(150);
+
+            GridLayout.LayoutParams params = (GridLayout.LayoutParams) textView.getLayoutParams();
+            params.setMargins(3, 3, 3, 3);
+            textView.setLayoutParams(params);
+
+            textView.setBackground(getResources().getDrawable(R.drawable.circle_unchecked));
+            textView.setTextSize(20);
+            textView.setTextColor(Color.BLACK);
+            textView.setText("");
+            textView.setGravity(android.view.Gravity.CENTER);
+            textView.setId(View.generateViewId());
+
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TextView clickedTextView = (TextView) v;
+                    if (clickedTextView.getBackground().getConstantState().equals(getResources().getDrawable(R.drawable.circle_unchecked).getConstantState())) {
+                        clickedTextView.setBackground(getResources().getDrawable(R.drawable.circle_checked));
+                        game[row][col] = 1;
+                    } else{
+                        clickedTextView.setBackground(getResources().getDrawable(R.drawable.circle_unchecked));
+                        game[row][col] = 0;
+                    }
+                    visualizarArrayEnLogcat();
+                }
+            });
+            gridLayout.addView(textView);
+        }
+        mainLayout.addView(gridLayout);
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        buttonParams.setMargins(100, 0, 0, 0);
+        goBack = new Button(this);
+        goBack.setLayoutParams(buttonParams);
+        goBack.setText("GO BACK");
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Game2Activity.this, MainActivity.class));
+            }
+        });
+        mainLayout.addView(goBack);
+        startGame();
+        setContentView(mainLayout);
+    }
+
+
+    private void visualizarArrayEnLogcat() {
+        StringBuilder arrayStr = new StringBuilder();
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                arrayStr.append(game[i][j]).append(" ");
+            }
+            arrayStr.append("\n");
+        }
+        Log.d("ArrayLog", arrayStr.toString());
+    }
 }
